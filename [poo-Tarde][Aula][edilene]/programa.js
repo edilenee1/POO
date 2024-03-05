bbb62857e9520f2a3b2c92bb4d9bd6d8b7bb6416
edilenee1 <159053117+edilenee1@users.noreply.github.com>
2024-03-05 16:26:12 -0300

    aula2

---
+++ [poo-Tarde][Aula][edilene]/programa.js
@@ -0,0 +1,82 @@
+let alunos = ['João','Maria','josé','Pedro','Paulo'];
+
+alert(alunos[3]);
+
+alunos[4] = 'Gerson'
+
+alunos.push('joaquim');
+
+let.notas  =[10,8,5,7,9,6];
+
+for(let i = 0; i< alunos.length;i++){
+    console.log(`Aluno: ${alunos[i]} -> Nota: ${notas[i]}`);
+}
+
+//criação de objetos
+
+let aluno1 = {
+    nome:'João',
+    cpf:'00000000000', 
+    nota:10
+}
+let aluno2={ 
+    nome:'maria',
+    cpf:'11111111111',
+    nota:8
+}
+
+let aluno3={
+    nome: 'José',
+    cpf:'22222222222',
+    nota:5
+}
+
+let aluno4={
+    nome:'Pedro',
+    cpf:'33333333333',
+    nota:7
+}
+let aluno5={
+    nome:'Paulo',
+    cpf:'44444444444',
+    nota:9
+}
+
+let aluno6={
+    nome:'Joaquim',
+    cpf:'55555555555',
+    nota:6
+}
+//Acesso de um atributos de objeto
+//em um array,a gente acessa um elemento através dos
+//colchetes [] e o índice.
+
+//Em um objeto,a gente usa.e o nome do atributo,Ex:
+console.log(aluno6.nome);
+console.log(aluno6.cpf);
+console.log(aluno6.nota);
+
+console.log(aluno1.nome);
+aluno1.nome = 'John';
+console.log(aluno1.nome);
+
+//Atividade carros
+let carro1={
+modelo:'altis',
+marca:'Toyota corolla',
+ano:'2020',
+quilometragem:'32.369',
+potenciaMotor:'8.0',
+cor:'branca',
+preços:'137.900',
+}
+
+let carro2={
+    modelo:
+}
+
+
+
+
+
+
